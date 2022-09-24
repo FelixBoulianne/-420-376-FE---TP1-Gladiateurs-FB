9341420f15d01eacd0719f2d1c851c6219ff3a61
ETI <ETI@192.168.0.30>
2022-09-24 18:05:55 -0400

    Fait jusqu'à l'étape 9 de la partie 1 (début de l'étape 10).

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -19,7 +19,7 @@ public class JeuGladiateurs {
     
     personnage1.Personnage("Bob le malchanceux", 70, 15, 15, 15);
     personnage2.Personnage("Igor l'empaleur", 100, 25, 5, 30);
-    
+            
     // </editor-fold>
 
     // **************************************************************************
@@ -38,6 +38,35 @@ public class JeuGladiateurs {
     // **************************************************************************
     // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
     // TODO : La boucle contenant les étapes du combat
+    
+        do {
+             tour.afficheTour();
+    
+        for (int i = 0; i <= 100; i++) {
+            
+            if (personnage1.getInitiative()== i) {
+                personnage1.frapperPersonnage(personnage2);
+            }
+            
+            if (personnage2.getInitiative()== i) {
+                personnage2.frapperPersonnage(personnage1);
+            }
+            
+        }
+        
+            affichage.afficherSeparateurInfosPerso();
+
+            personnage1.afficherInfosPersonnage();
+
+            personnage2.afficherInfosPersonnage();
+
+            tour.augmenteTour();
+
+            affichage.afficherSeparateurDeTour();
+      
+        } while (personnage1.getPointsDeVie() != 0 && personnage2.getPointsDeVie() != 0);
+    
+      
     // TODO : Après la boucle, afficher le résultat du combat
     // </editor-fold>
     }
